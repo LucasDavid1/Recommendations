fcf1adaa8b05843a6a7b7d03d9c682ca48f7dbf8
lucardo0 <40047353+lucardo0@users.noreply.github.com>
2019-07-19 11:06:14 -0400

    Add files via upload

---
+++ recommend_flask.py
@@ -0,0 +1,187 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from sklearn import preprocessing
+from lightfm import LightFM
+from scipy.sparse import csr_matrix 
+from scipy.sparse import coo_matrix 
+from sklearn.metrics import roc_auc_score
+import time
+from lightfm.evaluation import auc_score
+import pickle
+import re
+
+books = pd.read_csv('/home/lucas/flaskit/recommendations_model/Books/BX-Books.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+books.columns = ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'imageUrlS', 'imageUrlM', 'imageUrlL']
+users = pd.read_csv('/home/lucas/flaskit/recommendations_model/Books/BX-Users.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+users.columns = ['userID', 'Location', 'Age']
+ratings = pd.read_csv('/home/lucas/flaskit/recommendations_model/Books/BX-Book-Ratings.csv', sep=';', error_bad_lines=False, encoding="latin-1")
+ratings.columns = ['userID', 'ISBN', 'bookRating']
+
+# Data Cleaning
+
+books.drop(['imageUrlS', 'imageUrlM', 'imageUrlL'],axis=1,inplace=True)
+# Se reasignan los valores de la fila ISBN == '0789466953'
+books.loc[books.ISBN == '0789466953','yearOfPublication'] = 2000
+books.loc[books.ISBN == '0789466953','bookAuthor'] = "James Buckley"
+books.loc[books.ISBN == '0789466953','publisher'] = "DK Publishing Inc"
+books.loc[books.ISBN == '0789466953','bookTitle'] = "DK Readers: Creating the X-Men, How Comic Books Come to Life (Level 4: Proficient Readers)"
+# Se reasignan los valores de la fila ISBN == '078946697X'
+books.loc[books.ISBN == '078946697X','yearOfPublication'] = 2000
+books.loc[books.ISBN == '078946697X','bookAuthor'] = "Michael Teitelbaum"
+books.loc[books.ISBN == '078946697X','publisher'] = "DK Publishing Inc"
+books.loc[books.ISBN == '078946697X','bookTitle'] = "DK Readers: Creating the X-Men, How It All Began (Level 4: Proficient Readers)"
+# Se reasignan los valores de la fila ISBN == '2070426769'
+books.loc[books.ISBN == '2070426769','yearOfPublication'] = 2003
+books.loc[books.ISBN == '2070426769','bookAuthor'] = "Jean-Marie Gustave"
+books.loc[books.ISBN == '2070426769','publisher'] = "Gallimard"
+books.loc[books.ISBN == '2070426769','bookTitle'] = "Peuple du ciel, suivi de 'Les Bergers"
+# Ahora que estan bien los anos, se pasa a valores numericos
+books.yearOfPublication=pd.to_numeric(books.yearOfPublication, errors='coerce')
+# Se deja en NULL los anos 0 o mayores a 2006
+books.loc[(books.yearOfPublication > 2006) | (books.yearOfPublication == 0),'yearOfPublication'] = np.NAN
+# Estos NULL se rellenan con el promedio
+books.yearOfPublication.fillna(round(books.yearOfPublication.mean()), inplace=True)
+# Los publisher con nulos se rellenan con 'other'
+books.loc[(books.ISBN == '193169656X'),'publisher'] = 'other'
+books.loc[(books.ISBN == '1931696993'),'publisher'] = 'other'
+# Las edades menores a 5 y mayores a 90 se reemplazan por el promedio y se pasan a integer
+users.loc[(users.Age > 90) | (users.Age < 5), 'Age'] = np.nan
+users.Age = users.Age.fillna(users.Age.mean())
+users.Age = users.Age.astype(np.int32)
+#Se calzan las tablas para que tengan los mismos id de usuarios e items
+ratings_new = ratings[ratings.ISBN.isin(books.ISBN)]
+ratings = ratings[ratings.userID.isin(users.userID)]
+ratings_explicit = ratings_new[ratings_new.bookRating != 0]
+
+# Usuarios que hayan rankeado al menos 20 libros
+# Y libros que hayan sido rankeados por lo menos por 20 usuarios
+
+counts1 = ratings_explicit['userID'].value_counts()
+ratings_explicit = ratings_explicit[ratings_explicit['userID'].isin(counts1[counts1 >= 20].index)]
+counts = ratings_explicit['bookRating'].value_counts()
+ratings_explicit = ratings_explicit[ratings_explicit['bookRating'].isin(counts[counts >= 20].index)]
+
+
+######## Training ########
+# Divide el DataFrame en train y test trasnformandolos en una matriz de esparcion.
+# Tambien devuelve raw_train_df que es train en forma de dataframe.
+def informed_train_test(rating_df, train_ratio):
+    split_cut = np.int(np.round(rating_df.shape[0] * train_ratio))
+    train_df = rating_df.iloc[0:split_cut]
+    test_df = rating_df.iloc[split_cut::]
+    # Deja a los usuarios y libros que esten en train y en test
+    test_df = test_df[(test_df['userID'].isin(train_df['userID'])) & (test_df['ISBN'].isin(train_df['ISBN']))]
+    id_cols = ['userID', 'ISBN']
+    trans_cat_train = dict()
+    trans_cat_test = dict()
+    for k in id_cols:
+        cate_enc = preprocessing.LabelEncoder()
+        # Transforma todas las columnas a categorias numericas
+        trans_cat_train[k] = cate_enc.fit_transform(train_df[k].values)
+        trans_cat_test[k] = cate_enc.transform(test_df[k].values)
+
+    # --- Encode ratings:
+    cate_enc = preprocessing.LabelEncoder()
+    ratings = dict()
+    # Transforma la columna bookRating a categorica numerica
+    ratings['train'] = cate_enc.fit_transform(train_df.bookRating)
+    # La transforma sin hacerle fit (si se repite un valor, se deja, a diferencia de fit_transform)
+    ratings['test'] = cate_enc.transform(test_df.bookRating)
+
+    n_users = len(np.unique(trans_cat_train['userID']))
+    n_items = len(np.unique(trans_cat_train['ISBN']))
+
+
+    # class scipy.sparse.coo_matrix(arg1, shape=None, dtype=None, copy=False)
+    # arg1: (data,(row, col))
+    # coo_matrix toma los valores que hay en data y la ubica en una matriz con la 
+    # ubicacion correspondiente de (row, col)
+    train = coo_matrix((ratings['train'], (trans_cat_train['userID'], \
+                                                          trans_cat_train['ISBN'])) \
+                                      , shape=(n_users, n_items))
+    test = coo_matrix((ratings['test'], (trans_cat_test['userID'], \
+                                                        trans_cat_test['ISBN'])) \
+                                     , shape=(n_users, n_items))
+    # Retorna una matriz de esparcion, como la matriz de interaccion, pero con los parametros entregados.
+    return train, test, train_df
+
+# Se divide la data 80% train y 20% test
+train, test, raw_train_df = informed_train_test(ratings_explicit, .8)
+
+# Se calcula el area bajo la curva (AUC)
+start_time = time.time()
+# Se crea el modelo con la funcion loss='Weighted Approximated-Rank Pairwise (warp)'
+# WARP: Maximises the rank of positive examples by repeatedly sampling negative examples until rank violating one is found.
+model=LightFM(no_components=115,learning_rate=0.027,loss='warp')
+# Aqui se le entrega una matriz de esparcion (la mayoria de sus elementos son 0) y se entrena el modelo.
+model.fit(train,epochs=12,num_threads=4)
+###### Interaction Matrix
+
+user_item_matrix = raw_train_df.pivot(index='userID', columns='ISBN', values='bookRating')
+user_item_matrix.fillna(0, inplace = True)
+user_item_matrix = user_item_matrix.astype(np.int32)
+
+# Diccionario de usuarios e items
+def user_item_dikts(interaction_matrix, items_df):
+    user_ids = list(interaction_matrix.index)
+    user_dikt = {}
+    counter = 0 
+    for i in user_ids:
+        user_dikt[i] = counter
+        counter += 1
+
+    item_dikt ={}
+    for i in range(items_df.shape[0]):
+        item_dikt[(items_df.loc[i,'ISBN'])] = items_df.loc[i,'bookTitle']
+    
+    return user_dikt, item_dikt   
+
+# Se crean los diccionarios de usuario e item
+user_dikt, item_dikt = user_item_dikts(user_item_matrix, books)
+
+
+
+app = Flask(__name__)
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+# User Recommendations
+def similar_recommendation(user_id,model=model, interaction_matrix=user_item_matrix, user_dikt=user_dikt,item_dikt=item_dikt,threshold = 0,number_rec_items = 15):
+
+
+    n_users, n_items = interaction_matrix.shape
+    user_x = user_dikt[user_id]
+    scores = pd.Series(model.predict(user_x,np.arange(n_items))) # A .predict se le pasan los usuarios a quien 
+    # recomendar y los items para calcular la afinidad entre usuario-item
+    scores.index = interaction_matrix.columns #Se obtiene el score dela matriz de interaccion y se ordena
+    scores = list(pd.Series(scores.sort_values(ascending=False).index))
+    
+    known_items = list(pd.Series(interaction_matrix.loc[user_id,:][interaction_matrix.loc[user_id,:] > threshold].index).sort_values(ascending=False))
+    # Se obtienen los items con los que ya interactuo el usuario y valoro por sobre el threshold
+    scores = [x for x in scores if x not in known_items]
+    score_list = scores[0:number_rec_items]
+    known_items = list(pd.Series(known_items).apply(lambda x: item_dikt[x])) # Se aplica el diccionario para que aparezcan los nombres en lugar de los ids
+    scores = list(pd.Series(score_list).apply(lambda x: item_dikt[x]))
+
+    return jsonify({'Recomendaciones':scores})
+
+@app.route('/item/<ISBN>',methods=['GET'])
+# Recomendacion de un item para una lista de usuarios
+def users_for_item(ISBN,model=model,interaction_matrix=user_item_matrix,number_of_user=15):
+
+    n_users, n_items = interaction_matrix.shape
+    # x es una lista de los book_id
+    x = np.array(interaction_matrix.columns)
+    # A .predict se le pasan todos los usuarios y se calcula la afinidad con un item en especifico
+                                    # searchsorted busca en la lista x la posicion de ISBN
+                                    # np.repeat repite n_users veces el valor que resulte de searchsorted
+    scores = pd.Series(model.predict(np.arange(n_users), np.repeat(x.searchsorted(ISBN),n_users)))
+    # Se crea una lista con los usuarios con mayor score
+    user_list = list(interaction_matrix.index[scores.sort_values(ascending=False).head(number_of_user).index])
+    return jsonify({'Usuarios':user_list})
+
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
